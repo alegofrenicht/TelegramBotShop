87ed6516ba9b155e96c988cfcbb6cd665ab11de6
Alex <alexander-replyansky@yandex.ru>
2023-05-28 02:21:02 +0300

    final_project commit

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 from app import app
 from app.bot import bot
 
-webhook_url = 'https://alegofrenicht.github.io/TelegramBotShop/'
-bot.set_webhook(url=webhook_url)
 
 if __name__ == "__main__":
-    app.run()
+    bot.infinity_polling()
