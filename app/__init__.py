f14512695ff135b628e64c21f55d49aa01b4fb90
Alex <alexander-replyansky@yandex.ru>
2023-05-28 00:12:20 +0300

    final_project commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,6 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-db_info = {'host': 'dpg-chp6hiqk728ivvuoeipg-a',
-           'database': 'bot_database_m3ar',
-           'psw': 'QTTkWWI6j6uLLWRJmnfrRtKEv8PezBR6',
-           'user': 'postgresql',
-           'port': '5432'}
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:QTTkWWI6j6uLLWRJmnfrRtKEv8PezBR6@dpg-chp6hiqk728ivvuoeipg-a.oregon-postgres.render.com/bot_database_m3ar'
 
