f348661273a7c20abd8b9ea03d15c7eec4c8c6cf
Alex <alexander-replyansky@yandex.ru>
2023-06-10 18:07:16 +0300

    final_project commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from os import getenv
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+SQLALCHEMY_DATABASE_URI = getenv('DB_URI')
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:QTTkWWI6j6uLLWRJmnfrRtKEv8PezBR6@dpg-chp6hiqk728ivvuoeipg-a.oregon-postgres.render.com/bot_database_m3ar'
+    'SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
